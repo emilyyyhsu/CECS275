093e0fa8df3e932512d03a65e18a517f49afa7d5
ChaoticNerd <nat.f.kho@gmail.com>
2025-10-20 17:50:12 -0700

    Update Lab9
    Pushing my part in processInput.h and main.cpp

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -1,20 +1,56 @@
 #include "fontLibrary.h"
 #include "processInput.h"
 
+void cleaningGnome(unique_ptr<char[]> &userInputChars, string** binString);
+
 int main(void){
 
-    string userInput = getUserInput();
+   string userInput = getUserWord();
+    int userInputSize = userInput.length(); // rows
+    int isBolded = isBold(); // columns
+    
+    string** binString;
+    binString = new string*[userInputSize]; // this is the rows
+    for (int i = 0; i < userInputSize; ++i) {
+        binString[i] = new string[isBolded];
+        for(int j = 0; j < isBolded; j++){ // this runs 0 to 7
+            binString[i][j] = "N";
+        cout << " CHECKING BINSTRING at i: " << i << " and j: " << j << " " << binString[i][j] << endl;
 
-    // holding space for null char
-    char* userInputChars = new char[userInput.length() + 1];
+        }
+        // cout << binString[i] << " CHECKING BINSTRING i" << endl;
+    }
 
-    processUserInput(userInput, userInputChars); // this function call breaks :: char* is a nullptr
 
-    /*///////////////////////////////// debug/testing //////////////////////////////////
-    cout << "*userInputChars produces: "    << *userInputChars      << endl; // this prints h (first char)
-    cout << "userInputChars produces "      << userInputChars       << endl; // this prints whatever is in userInput
-    cout << "userInputChars[3] produces "   << userInputChars[3]    << endl; // this prints whatever is in third index of userInput
-    //////////////////////////////////////////////////////////////////////////////////*/
+    
+    // // holding space for null char
+    unique_ptr<char[]> userInputChars( new char[userInputSize + 1]);
+    // cout << "FUCK MY STUPID BAKA LIFE" << endl;
 
+    processUserInput(userInput, userInputChars); 
+
+    // // cout << "\u001b[8;42m   00011101010  \u001b[0m" << endl;
+    // // cout << ESCAPE_CHARACTER << HIDE << ';' << GREEN_BACKGROUND  << " ASDJKHASKJDAS " << ESCAPE_CHARACTER <<"0m" << endl;
+    user2Bin(userInputChars, userInputSize, binString);
+
+
+    showPixels(binString, isBolded, userInputSize);
+    // cout << "HOT MILFS IN YOUR AREA " << endl;
+
+    ///////////////////////////////// debug/testing //////////////////////////////////
+    // cout << "userInputChars.get() produces: "       << userInputChars.get()       << endl; // this prints whole thing (converts into string?)
+    // cout << "userInputChars.get()[1] produces: "    << userInputChars.get()[1]    << endl; // this prints element in index 1
+    // cout << "userInputChars[3] produces "   << *userInputChars[3]    << endl; // this prints whatever is in third index of userInput
+    //////////////////////////////////////////////////////////////////////////////////
+
+    // delete all :: begoneTHOT()
+    cleaningGnome(userInputChars, binString);
     return 0;
+}
+
+void cleaningGnome(unique_ptr<char[]> &userInputChars, string** binString){
+    userInputChars.reset();
+
+    for(int i = 0; i < sizeof(binString)/sizeof(binString[0]); i++ )
+        delete [] binString[i];
 }
--- Lab9/processInput.h
+++ Lab9/processInput.h
@@ -1,22 +1,40 @@
 #include <array>
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
-// Function prototypes
-string getUserInput(void);
-char* processUserInput(string userInput);
+// Constant declarations
+const string ESCAPE_CHARACTER = "\u001b[";
+const string HIDE = "8";
+const string GREEN_BACKGROUND = "42m";
+const string WHITE_BACKGROUND = "47m";
+const string RESET_CHARACTER = "0m";
 
+// Function prototypes
+string getUserWord(void);
+void processUserInput(string userInput, unique_ptr<char>& userInputChars);
+void showPixels(string** userInputBin);
 /*
  * @author: Natasha Kho
  * @return: string containing user's input
  * @purpose: gets user input (can be ran multiple times if necessary)
 */
-string getUserInput(void){
+string getUserWord(void){
     string userInput;
     cout << "Hello! Please type what you want to display." << endl;
     cin >> userInput;
     return userInput;
+}
+
+int isBold(void){
+    string balls;
+    cout << "Do you want your word bolded? (Y/N)" << endl;
+    cin >> balls;
+    if(balls == "Y"){
+        return 8;
+    }
+    return 5;
 }
 
 /*
@@ -24,20 +42,42 @@ string getUserInput(void){
  * @return: char pointer showing where the 
             first location of the userInput broken up into individual
             chars is located
- * @purpose: breaks down user's input into char array and returns 
+ * @brief: breaks down user's input into char array and returns 
             pointer to initial location
  * Honestly, never do this. literally just use a string variable.
 */
-char* processUserInput(string userInput, char* userInputChars){
+void processUserInput(string userInput, unique_ptr<char[]>& userInputChars){
+    // allocate buffer with space for null terminator
+    //userInputChars = make_unique<char[]>(userInput.length() + 1);
     
     // parsing user string into char*
     for(int i = 0; i < userInput.length(); i++){
         userInputChars[i] = userInput[i];
     }
-
     // null for last character
     userInputChars[userInput.length() + 1] = '\0';
 
-    // returns char*
-    return userInputChars;
+}
+
+// BullrOX
+/*
+ * @author: Natasha Kho
+ # @param: User's input as binary string, ready to print
+ * @brief: iterates through binary string and prints it out accordingly
+*/
+void showPixels(string** userInputBin, int column, int row){
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            // cout << ESCAPE_CHARACTER << HIDE << ';' << GREEN_BACKGROUND  << " ASDJKHASKJDAS " << ESCAPE_CHARACTER <<"0m" << endl;
+            // cout << userInputBin[i][j] << endl;
+            for(int k = 0; k < userInputBin[i][j].length(); k++){
+                if(userInputBin[i][j][k] == '1')
+                    cout << ESCAPE_CHARACTER << HIDE << ';' << GREEN_BACKGROUND << " " << ESCAPE_CHARACTER << RESET_CHARACTER;
+                else
+                    cout << ESCAPE_CHARACTER << HIDE << ';' << WHITE_BACKGROUND << " " << ESCAPE_CHARACTER << RESET_CHARACTER;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 }
